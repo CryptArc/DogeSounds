44384bb56cbd8155e93980626adbe411f50b98e6
Thomas Gillis <thomaswgillis@gmail.com>
2014-05-20 23:56:27 -0400

    dirac

--- api.php
+++ api.php
@@ -75,6 +75,8 @@ if (isset($_GET["request"])){
     "dgb"=>"http://cryptexplorer.com/chain/DigiByte/q/",
     "ltc"=>"http://explorer.litecoin.net/chain/Litecoin/q/",
     "btc"=>"https://blockexplorer.com/q/",
+    "xdq"=>"http://diracexplorer.org:2750/chain/Dirac/q/",
+
     "zet"=>"http://petroexplorer.info/chain/zetacoin/q/");
   $api_url = $apis[$chain];
   $req = $_GET["request"];
